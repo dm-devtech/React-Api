7f0f4b53a3db127eeacec2843b17549a23640f43
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-01-27 16:56:38 +0000

    added first react test and started building data table

--- react-with-api/src/App.test.js
+++ react-with-api/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders api test data', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/michael/i);
   expect(linkElement).toBeInTheDocument();
 });
